161ef9001b3f7ae5b40aac4944ea135449ab605b
Julio Perez <julioperez.dev@gmail.com>
2023-10-12 10:18:56 -0300

    change porte to test with docker

--- next.config.js
+++ next.config.js
@@ -2,8 +2,8 @@
 const nextConfig = {
     output:'standalone',
     env: {
-        apiUrl: 'http://localhost:8081/api', //LOCAL
-        //apiUrl: 'http://localhost:18081/api', //PROD
+        //apiUrl: 'http://localhost:8081/api', //LOCAL
+        apiUrl: 'http://localhost:18081/api', //PROD
       },
 }
 
