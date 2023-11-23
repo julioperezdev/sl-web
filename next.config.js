68ef5a4959d67f15fafe1dda68788cf9808eb259
Julio Perez <julioperez.dev@gmail.com>
2023-11-23 01:35:14 -0300

    adding new production IP

--- next.config.js
+++ next.config.js
@@ -3,7 +3,8 @@ const nextConfig = {
     output:'standalone',
     env: {
         //apiUrl: 'http://localhost:8081/api', //LOCAL
-        apiUrl: 'http://localhost:18081/api', //PROD
+        //apiUrl: 'http://localhost:18081/api', //PROD
+        apiUrl: 'http://191.101.71.56:18081/api', //PROD
       },
 }
 
